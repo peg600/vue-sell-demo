1ba2b8fa325669a04b77d863b39810074abfa1c4
peg600 <peg600@sina.comom>
2018-03-30 04:31:22 +0800

    3.30

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
   <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content">
+      i am content
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
+  import vheader from './components/header/header.vue'
+
+  export default {
+    name: 'App',
+    components:{
+     'v-header': vheader
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    .tab
+      display:flex
+      width :100%
+      height :40px
+      line-height :40px
+      .tab-item
+        flex :1
+        text-align :center
+
+
 </style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header">我是header</div>
+</template>
+
+<script>
+    export default {
+        name: "vheader"
+    }
+</script>
+
+<style type="text/ecmascript-6" lang="stylus">
+
+</style>
