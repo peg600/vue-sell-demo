5f68652966ec3051a0def50a1b9cecaf78455038
peg600 <peg600@sina.com>
2018-04-12 04:54:09 +0800

    4.12.1

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,13 +1,55 @@
 <template>
-  <div>i am goods</div>
+  <div class="goods">
+    <div class="menu-wrapper"></div>
+    <div class="foods-wrapper"></div>
+  </div>
+
 </template>
 
 <script>
+
+  import axios from 'axios'
+
+  const ERR_OK = 0;
+
     export default {
-        name: "goods"
+      name: "goods",
+      props:{
+        seller:{
+          type: Object
+        }
+      },
+      data() {
+        return {
+          goods: []
+        };
+      },
+      created() {
+        axios.get('/api/goods').then((response) => {
+          response = response.data;
+          if (response.errno === ERR_OK) {
+            this.goods = response.data;
+            console.log(this.goods);
+          }
+        });
+      }
     }
 </script>
 
-<style scoped>
+<style lang="stylus" rel="stylesheet/stylus">
+
+  .goods
+    display: flex
+    position: absolute
+    width: 100%
+    top: 174px
+    bottom: 46px
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px
+      background: #f3f5f7
+    .foods-wrapper
+      flex: 1
 
 </style>
