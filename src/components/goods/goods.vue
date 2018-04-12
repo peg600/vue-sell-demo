ac4d91766cb52ed1536532b843de58f3608d9811
peg600 <peg600@sina.com>
2018-04-13 04:46:14 +0800

    4.13
    scroll

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,14 +1,51 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper"></div>
-    <div class="foods-wrapper"></div>
+    <div class="menu-wrapper" ref="menuWrapper">
+    <!-- ref用于注册子组件或子元素，注册的组件将会出现在它的父组件的$refs对象中，父组件可通过$refs访问子组件和其中的数据
+    如：father.$refs.子组件ref属性的值.子组件中的属性或方法。
+    注：ref是作为渲染结果被创建的，渲染初期不存在也无法访问；$refs不是响应式的，不可用于数据绑定 -->
+      <ul>
+        <li v-for="(item,index) in goods" class="menu-item" :class="{'current': currentIndex===index}" ref="menuList">
+          <span class="text border-1px">
+            <span class="icon" v-show="item.type>0" :class="classMap[item.type]"></span>
+            {{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper" ref="foodsWrapper">
+      <ul>
+        <li v-for="item in goods" class="food-list" ref="foodList">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item border-1px)">
+              <div class="icon">
+                <img :src="food.icon" width="57" height="57">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span v-show="food.oldPrice" class="old">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 
 </template>
 
 <script>
 
-  import axios from 'axios'
+  import axios from 'axios';
+  import BScroll from 'better-scroll';
 
   const ERR_OK = 0;
 
@@ -21,22 +58,69 @@
       },
       data() {
         return {
-          goods: []
+          goods: [],
+          listHeight: [],
+          scrollY: 0
         };
       },
+      computed: {
+        currentIndex() {
+          for(let i=0;i<this.listHeight.length;i++) {
+            let height1 = this.listHeight[i];
+            let height2 = this.listHeight[i+1];
+            if(!height2 || (this.scrollY > height1 && this.scrollY < height2)) {
+              return i;
+            }
+          }
+          return 0;
+        }
+      },
       created() {
+        this.classMap = ['decrease','discount','special','invoice','guarantee'];
+
         axios.get('/api/goods').then((response) => {
           response = response.data;
           if (response.errno === ERR_OK) {
             this.goods = response.data;
-            console.log(this.goods);
+            this.$nextTick(() => {    // $nextTick会在下次DOM更新循环结束之后执行延迟回调，即this._initScroll会在DOM更新
+              this._initScroll();     // 后执行，避免初期渲染时就执行导致$refs中的内容无法访问
+              this._calculateHeight();
+            });
           }
         });
+      },
+      methods:{
+        _initScroll() {
+          this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+            click: true
+          });
+
+          this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+            click: true,
+            probeType: 3
+          });
+
+          this.foodsScroll.on('scroll',(pos) => {
+            this.scrollY = Math.abs(Math.round(pos.y));
+          })
+        },
+        _calculateHeight() {
+          let foodList = this.$refs.foodList;
+          let height = 0;
+          this.listHeight.push(height);
+          for (let i = 0; i < foodList.length; i++) {
+            let item = foodList[i];
+            height += item.clientHeight;
+            this.listHeight.push(height);
+          }
+        }
       }
     }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+
+  @import "../../common/stylus/mixin.styl"
 
   .goods
     display: flex
@@ -49,7 +133,119 @@
       flex: 0 0 80px
       width: 80px
       background: #f3f5f7
+      .menu-item
+        display: table
+        height: 54px
+        width: 56px
+        padding: 0 12px
+        line-height: 14px
+        &.current
+          position: relative
+          z-index: 10
+          margin-top: -1px
+          background: #fff
+          font-weight: 700
+          .text
+            border: none
+        .icon
+          display: inline-block
+          vertical-align: top
+          width: 12px
+          height: 12px
+          margin-right: 2px
+          background-size: 12px 12px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+        .text
+          display: table-cell
+          width: 56px
+          vertical-align: middle
+          border-1px(rgba(1,17,27,0.1))
+          font-size: 12px
+
     .foods-wrapper
       flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9dde1
+        font-size: 12px
+        color: rgb(147,153,159)
+        background: #f3f5f7
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        border-1px(rgba(1,17,27,0.1))
+        &:last-child
+          border-none()
+          margin-bottom: 0
+        .icon
+          flex: 0 0 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .name
+            margin: 2px 0 8px 0
+            line-height: 14px
+            font-size: 14px
+            color: rgb(7,17,27)
+          .desc,.extra
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147,153,159)
+          .desc
+            margin-bottom: 8px
+            line-height: 12px
+          .extra
+            .count
+              margin-right: 12px
+          .price
+            font-weight: 700
+            line-height: 24px
+            .now
+              margin-right: 8px
+              font-size: 14px
+              color: rgb(240,20,20)
+            .old
+              text-decoration: line-through
+              font-size: 10px
+              color: rgb(147,153,159)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 </style>
