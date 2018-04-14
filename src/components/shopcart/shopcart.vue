f6a59c6313b6c2e21615edf3b5937769510cdf87
peg600 <peg600@sina.com>
2018-04-15 03:33:41 +0800

    4.15

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,15 +3,16 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <i class="icon-shopping_cart"></i>
+          <div class="logo" :class="{'highlight': totalCount>0}">
+            <i class="icon-shopping_cart" :class="{'highlight': totalCount>0}"></i>
           </div>
+          <div class="num" v-show="totalCount>0">{{totalCount}}</div>
         </div>
-        <div class="price">000</div>
+        <div class="price" :class="{'highlight': totalPrice>0}">￥{{totalPrice}}</div>
         <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
       </div>
       <div class="content-right">
-
+        <div class="pay" :class="payClass">{{payDesc}}</div>
       </div>
     </div>
   </div>
@@ -21,6 +22,17 @@
     export default {
       name: "shopcart",
       props: {
+        selectFoods: {
+          type: Array,
+          default() {             // 当属性type为Array或Object时，default应为一个函数
+            return [
+              {
+                price: 10,
+                count: 2
+              }
+            ];
+          }
+        },
         deliveryPrice: {
           type: Number,
           default: 0
@@ -29,7 +41,41 @@
           type: Number,
           default: 0
         }
+      },
+      computed:{
+        totalPrice() {
+          let total = 0;
+          this.selectFoods.forEach((food) => {
+            total += food.price * food.count;
+          });
+          return total
+        },
+        totalCount() {
+          let count = 0;
+          this.selectFoods.forEach((food) => {
+            count += food.count;
+          });
+          return count;
+        },
+        payDesc() {
+          if(this.totalPrice === 0) {
+            return `￥${this.minPrice}元起送`;      // 反引号`为ES6中用于拼接字符串，其中的变量用${}包围
+          }else if(this.totalPrice < this.minPrice) {
+            let diff = this.minPrice - this.totalPrice;
+            return  `还差￥${diff}元起送`;
+          }else{
+            return "去结算";
+          }
+        },
+        payClass() {
+          if(this.totalPrice < this.minPrice) {
+            return "not-enough";
+          }else {
+            return "enough";
+          }
+        }
       }
+
     }
 </script>
 
@@ -44,6 +90,7 @@
     .content
       display: flex
       background: #141d27
+      color: rgba(255,255,255,0.4)
       .content-left
         flex: 1
         font-size: 0
@@ -65,10 +112,28 @@
             border-radius: 50%
             background: #2b343c
             text-align: center
+            &.highlight
+              background: rgb(0,160,220)
             .icon-shopping_cart
               font-size: 24px
               color: #80858a
               line-height: 44px
+              &.highlight
+                color: #fff
+          .num
+            position: absolute
+            top: 0
+            right: 0
+            width: 24px
+            height: 16px
+            line-height: 16px
+            text-align: center
+            border-radius: 16px
+            font-size: 9px
+            font-weight: 700
+            color: white
+            background: rgb(240,20,20)
+            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.4)
         .price
           display: inline-block
           vertical-align: top
@@ -79,16 +144,27 @@
           border-right: 1px solid rgba(255,255,255,0.1)
           font-size: 16px
           font-weight: 700
-          color: rgba(255,255,255,0.4)
+          &.highlight
+            color: white
         .desc
           display: inline-block
           vertical-align: top
           margin: 12px 0 0 12px
           line-height: 24px
           font-size: 10px
-          color: rgba(255,255,255,0.4)
       .content-right
         flex: 0 0 105px
         width: 105px
-
+        .pay
+          height: 48px
+          line-height: 48px
+          text-align: center
+          font-size: 12px
+          font-weight: 700
+          background: #2b333b
+          &.not-enough
+            background: #2b333b
+          &.enough
+            background: #00b43c
+            color: white
 </style>
