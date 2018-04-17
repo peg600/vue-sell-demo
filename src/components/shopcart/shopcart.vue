3ac4c82e9960b63cb8226723dcbba5df46fedaec
peg600 <peg600@sina.com>
2018-04-18 02:48:32 +0800

    4.18

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="shopcart">
-      <div class="content">
+      <div class="content" @click="toggleList">
         <div class="content-left">
           <div class="logo-wrapper">
             <div class="logo" :class="{'highlight': totalCount>0}">
@@ -27,6 +27,27 @@
           </transition>
         </div>
       </div>
+      <transition name="fold">
+        <div class="shopcart-list" v-show="listShow">
+          <div class="list-header">
+            <h1 class="title">购物车</h1>
+            <span class="empty">清空</span>
+          </div>
+          <div class="list-content" ref="listContent">
+            <ul>
+              <li class="food" v-for="food in selectFoods">
+                <span class="name">{{food.name}}</span>
+                <div class="price">
+                  <span>￥{{food.price * food.count}}</span>
+                </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food"></cartcontrol>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </transition>
     </div>
   </div>
 
@@ -36,6 +57,7 @@
 
   import BScroll from 'better-scroll';
   import cartcontrol from "../cartcontrol/cartcontrol";
+
 
     export default {
       props: {
@@ -77,7 +99,8 @@
               show: false
             }
           ],
-          dropBalls: []
+          dropBalls: [],
+          fold: true
         };
       },
       computed:{
@@ -111,6 +134,25 @@
           }else {
             return "enough";
           }
+        },
+        listShow() {
+          if(!this.totalCount) {
+            this.fold = true;
+            return false;
+          }
+          let show = !this.fold;
+          if(show) {
+            this.$nextTick(() => {
+              if(!this.scroll) {
+                this.scroll = new BScroll(this.$refs.listContent,{
+                  click: true
+                });
+              }else {
+                this.scroll.refresh();
+              }
+            });
+          }
+          return show;
         }
       },
       methods: {
@@ -159,6 +201,12 @@
             ball.show = false;
             el.style.display = 'none';
           }
+        },
+        toggleList() {
+          if(!this.totalCount) {
+            return;
+          }
+          this.fold = !this.fold;
         }
       },
       components: {
@@ -168,6 +216,9 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+
+  @import "../../common/stylus/mixin.styl"
+
   .shopcart
     position: fixed
     left: 0
@@ -267,4 +318,80 @@
           border-radius: 50%
           background: rgb(0,160,220)
           transition: all 0.4s linear
+    .shopcart-list
+      position :absolute
+      top: 0
+      left: 0
+      z-index: -1
+      width: 100%
+      transform:translate3d(0,-100%,0)
+      &.fold-enter-active, &.fold-leave-active
+        transition: all 0.5s
+      &.fold-enter, &.fold-leave-active
+        transform: translate3d(0,0,0)
+      .list-header
+        height: 40px
+        line-height: 40px
+        padding: 0 18px
+        background: #f3f5f7
+        border-bottom: 1px solid rgba(7,17,27,0.1)
+        .title
+          float: left
+          font-size: 14px
+          color: rgb(7,17,27)
+        .empty
+          float: right
+          font-size: 14px
+          color: rgb(0,160,220)
+
+
+      .list-content
+        padding: 0 18px
+        max-height: 217px
+        overflow: hidden
+        background: #fff
+        .food
+          position: relative
+          padding: 12px 0
+          box-sizing: border-box
+          border-1px(rgba(7, 17, 27, 0.1))
+          .name
+            line-height: 24px
+            font-size: 14px
+            color: rgb(7, 17, 27)
+          .price
+            position: absolute
+            right: 90px
+            bottom: 12px
+            line-height: 24px
+            font-size: 14px
+            font-weight: 700
+            color: rgb(240, 20, 20)
+          .cartcontrol-wrapper
+            position: absolute
+            right: 0
+            bottom: 6px
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </style>
